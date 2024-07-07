a95af9edcda097acc36906aa2a22158aaa34de35
Aleksei Emelianov <aemelianov@rexalto.tech>
2024-07-07 15:31:38 +0200

    refactoring

--- front/src/main/java/skyhawk/test/task/util/DatabaseUtil.java
+++ front/src/main/java/skyhawk/test/task/util/DatabaseUtil.java
@@ -33,8 +33,8 @@ public class DatabaseUtil {
 
         try (ResultSet resultSet = statement.executeQuery()) {
           while (resultSet.next()) {
-            Integer i;
-            Double d;
+            int i;
+            double d;
             res.add(new CacheRecord(
                 new Log(
                     resultSet.getString("season"),
